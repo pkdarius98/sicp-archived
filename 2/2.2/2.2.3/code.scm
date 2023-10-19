a722e425ea9a797221e908740bd6aef6e75e9a61
Trinh Duy Hai <pkdarius98@gmail.com>
2023-10-19 15:55:31 +0700

    temp

--- 2/2.2/2.2.3/code.scm
+++ 2/2.2/2.2.3/code.scm
@@ -63,3 +63,24 @@
 ; (define (salary_of_highest_paid_programmer records)
 ;     (accumulate max 0 (map salary (filter programmer? records))))
 
+                
+
+    (iter (car positions) (cdr positions)))
+
+(define (queens board_size)
+    (define (queen_cols k)
+        (if (= k 0)
+            (list '())
+            (filter
+                (lambda (positions)
+                    (safe? k positions)
+                    )
+                (flatmap
+            (lambda (new_row)
+                (map (lambda (rest_of_queens)
+                    (adjoin_position 
+                    new_row k rest_of_queens))
+                    (queen_cols (- k 1))))
+            (enumerate_interval 1 board_size)))))
+                (queen_cols board_size))
+(length (queens 8))
